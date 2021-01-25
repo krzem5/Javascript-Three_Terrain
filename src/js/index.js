70d9d6b3d4702dd1a7f01bfb6c44db9c22dbeaaf
Krzem <krzem5.dev@gmail.com>
2021-01-25 19:15:53 +0100

    Push Update 01/25/2021, 19:15:48

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-var scene,cam,renderer,controls
+var scene,cam,renderer,controls,ambient,all_objects=[],NOISE=new ImprovedNoise().noise,SEED=Math.random(),SIZE=1
 function init(){
 	scene=new THREE.Scene()
 	cam=new THREE.PerspectiveCamera(60,window.innerWidth/window.innerHeight,0.1,100000)
@@ -9,16 +9,30 @@ function init(){
 	renderer.setSize(window.innerWidth,window.innerHeight)
 	scene.background=new THREE.Color().setHSL(1,1,1)
 	document.body.appendChild(renderer.domElement)
-	ambient=new THREE.AmbientLight(0xffffff,1)
+	ambient=new THREE.AmbientLight(0xececec,1)
 	scene.add(ambient)
+	var point=new THREE.SpotLight(0xffffff,1,0,1)
+	point.position.set(17500,2000,0)
+	point.target.position.set(0,0,0)
+	point.castShadow=true
+	point.shadow.mapSize.width=1024
+	point.shadow.mapSize.height=1024
+	point.shadow.camera.near=0.5
+	point.shadow.camera.far=15000
+	// scene.add(point)
+	// scene.add(point.target)
 	renderer.render(scene,cam)
 	controls=new THREE.OrbitControls(cam,renderer.domElement)
 	controls.target=new THREE.Vector3(0,0,0)
 	window.addEventListener("resize",resize,false)
 	window.addEventListener("keypress",onkeypress)
 	requestAnimationFrame(render)
+	generate()
 }
 function render(){
+	for (var k of all_objects){
+		k.update()
+	}
 	renderer.render(scene,cam)
 	requestAnimationFrame(render)
 }
@@ -28,8 +42,14 @@ function resize(){
 	renderer.setSize(window.innerWidth,window.innerHeight)
 }
 function onkeypress(e){
-	switch (e.keyCode){
-		//
-	}
 }
-document.addEventListener("DOMContentLoaded",init,false)
+function generate(){
+	all_objects.push(Terrain())
+}
+Number.prototype.map=function(as,ae,bs,be){
+	return (this-as)*(be-bs)/(ae-as)+bs
+}
+Number.prototype.mapC=function(as,ae,c1,c2){
+	return [this.map(as,ae,c1[0]/360,c2[0]/360),this.map(as,ae,c1[1]/100,c2[1]/100),this.map(as,ae,c1[2]/100,c2[2]/100)]
+}
+window.addEventListener("DOMContentLoaded",init,false)
---
+++ src/js/terrain.js
@@ -0,0 +1,53 @@
+function Terrain(){
+	var g=new THREE.Group()
+	g.rotation.x=Math.PI/2
+	for (var y=0;y<SIZE;y++){
+		for (var x=0;x<SIZE;x++){
+			var c=Chunk(x-Math.floor(SIZE/2),y-Math.floor(SIZE/2))
+			g.add(c.object)
+			all_objects.push(c)
+		}
+	}
+	scene.add(g)
+	return {
+		type: "terrain",
+		chunkList: g,
+		update: function(){
+		}
+	}
+}
+function get_noise(x,y){return NOISE(x,y,SEED)*600}
+function Chunk(x,y){
+	var mt=new THREE.MeshStandardMaterial({color:0xffffff,side:THREE.DoubleSide,flatShading:true,metalness:0.5,roughness:0.9,vertexColors:THREE.VertexColors})
+	var mesh=new THREE.Mesh(new THREE.PlaneGeometry(1000,1000,10,10),mt)
+	mesh.material.vertexColors=THREE.FaceColors
+	mesh.reciveShadow=true
+	mesh.castShadow=true
+	mesh.position.set(x*1000,y*-1000,0)
+	scene.add(mesh)
+	for (var j=0;j<11;j++){
+		for (var i=0;i<11;i++){
+			mesh.geometry.vertices[j*11+i].z=get_noise(x+i/10,y+j/10)
+		}
+	}
+	p=0
+	for (var f of mesh.geometry.faces){
+		y=mesh.geometry.vertices[Math.floor(p)].z
+		var fi=mesh.geometry.faces.indexOf(f)
+		for (var i=0;i<3;i++){
+			// r=fi.map(0,mesh.geometry.faces.length,0,1)
+			r=y.map(-600,600,0,1)
+			f.vertexColors[i]=new THREE.Color(r,r,r)
+		}
+		p+=0.5
+	}
+	mesh.geometry.faces[0].vertexColors=[new THREE.Color(0xff0000),new THREE.Color(0x00ff00),new THREE.Color(0x0000ff)]
+	return {
+		type: "chunk",
+		object: mesh,
+		v: true,
+		update: function(){
+			this.object.visible=this.v
+		}
+	}
+}
